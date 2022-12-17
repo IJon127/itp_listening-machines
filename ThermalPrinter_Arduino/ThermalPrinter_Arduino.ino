fd428840f2d076ab151b91659c3c663afeea2ce7
IJon127 <ijon.hsieh@gmail.com>
2022-12-16 23:50:18 -0500

    readBtyeUntil

--- ThermalPrinter_Arduino/ThermalPrinter_Arduino.ino
+++ ThermalPrinter_Arduino/ThermalPrinter_Arduino.ino
@@ -19,42 +19,48 @@ Modified: Dec 16, 2022
 SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
 Adafruit_Thermal printer(&mySerial);     // Pass addr to printer constructor
 
-//char density[10] = {' ', '.', '•', ':', ';', '░', '▒', '▓', '█'};
-char density[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+//char density[9] = {' ', '.', '•', ':', ';', '░', '▒', '▓', '█'};
+//char density[9] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+char density[11] = {' ', '.', ':', '-', 'i', '|', '=', '+', '%', 'O', '#'};
+
 //const density = " .•:;░▒▓█";
+//W$9876543210?!abc;:+=-,._ 
+//  .:-i|=+%O#
 
 void setup() {
 // NOTE: SOME PRINTERS NEED 9600 BAUD instead of 19200, check test page.
   mySerial.begin(19200);  // Initialize SoftwareSerial
   Serial.begin(19200);    // Initialize serial communications
-  Serial.setTimeout(10);
+  Serial.setTimeout(100);
   // Serial.setTimeout(10); // Set the timeout for parseInt
   printer.begin();        // Init printer (same regardless of serial type)
 
-  //printer.setFont('B');   // Set font to style 'B'
+  printer.setFont('B');   // Set font to style 'B'
   printer.setSize('S');   // Set font size to smallest
   printer.setLineHeight(24);  // Set line height to 24px
+  printer.justify('C');
 
   printer.println("ijon test"); // Printer print text: "cheeseTest"
   //printer.setDefault(); // Restore printer to defaults
 }
 
 void loop() {
-    //Serial.flush();
+    Serial.flush();
     if (Serial.available() > 0) {
       // printer.println("serial available!");
-      int inByteArr[12];
-      for(int n=0; n<12; n++){
-        inByteArr[n] = Serial.read();
+      char inByteArr[12];
+      int size = Serial.readBytesUntil('\n', inByteArr, 12);
+      for(char n=0; n<size; n++){
+        //inByteArr[n] = Serial.read();
 
-        const int len = 10;
-        const int charIndex = floor(map(inByteArr[n], 0, 255, 0, len));
-        printer.print(density[charIndex]);
-        printer.print(density[charIndex]);
-
-
-
+        const char len = 11;    // Number reflects density array
+        const char charIndex = floor(map(inByteArr[n], 0, 255, 0, len));
         //printer.print(inByteArr[n]);
+        //printer.print(charIndex);
+        printer.print(density[charIndex]);
+        printer.print(density[charIndex]);
+        printer.print(density[charIndex]);
+        
       }
 
       printer.println();
