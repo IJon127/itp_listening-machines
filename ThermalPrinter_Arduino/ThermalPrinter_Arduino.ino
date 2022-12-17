cfb6c14d8bd923116e3cf13c89792b648aefb1d9
IJon127 <ijon.hsieh@gmail.com>
2022-12-16 22:42:25 -0500

    add arduino code

---
+++ ThermalPrinter_Arduino/ThermalPrinter_Arduino.ino
@@ -0,0 +1,63 @@
+/* Title: Thermal Printer Communicate with JS via p5.webserial
+Source: 
+Tom Igoe PComp Lab: Serial Output from P5.js using p5.webserial Library
+<https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-webserial-output-from-p5-js/>
+Description:
+JS communicate Arduino via P5.webserial and print to printer
+Created: Dec 16, 2022
+Modified: Dec 16, 2022
+*/
+
+#include "Adafruit_Thermal.h" // Thermal Printer library
+// Here's the new syntax when using SoftwareSerial (e.g. Arduino Uno) ----
+// If using hardware serial instead, comment out or remove these lines:
+
+#include "SoftwareSerial.h"
+#define TX_PIN 6 // Arduino transmit  YELLOW WIRE  labeled RX on printer
+#define RX_PIN 5 // Arduino receive   GREEN WIRE   labeled TX on printer
+
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
+Adafruit_Thermal printer(&mySerial);     // Pass addr to printer constructor
+
+//char density[10] = {' ', '.', '•', ':', ';', '░', '▒', '▓', '█'};
+char density[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+//const density = " .•:;░▒▓█";
+
+void setup() {
+// NOTE: SOME PRINTERS NEED 9600 BAUD instead of 19200, check test page.
+  mySerial.begin(19200);  // Initialize SoftwareSerial
+  Serial.begin(19200);    // Initialize serial communications
+  Serial.setTimeout(10);
+  // Serial.setTimeout(10); // Set the timeout for parseInt
+  printer.begin();        // Init printer (same regardless of serial type)
+
+  //printer.setFont('B');   // Set font to style 'B'
+  printer.setSize('S');   // Set font size to smallest
+  printer.setLineHeight(24);  // Set line height to 24px
+
+  printer.println("ijon test"); // Printer print text: "cheeseTest"
+  //printer.setDefault(); // Restore printer to defaults
+}
+
+void loop() {
+    //Serial.flush();
+    if (Serial.available() > 0) {
+      // printer.println("serial available!");
+      int inByteArr[12];
+      for(int n=0; n<12; n++){
+        inByteArr[n] = Serial.read();
+
+        const int len = 10;
+        const int charIndex = floor(map(inByteArr[n], 0, 255, 0, len));
+        printer.print(density[charIndex]);
+        printer.print(density[charIndex]);
+
+
+
+        //printer.print(inByteArr[n]);
+      }
+
+      printer.println();
+
+    }
+}
